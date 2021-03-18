9bfc8eddc39a0f6297ebc6bd2da71bf3c701b25a
rowjay007 <rowlives@gmail.com>
2021-03-18 14:45:03 +0100

    create subscribe and getState in store

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-function createStore () {
+function createStore() {
   // The store should have four parts
   // 1. The state
   // 2. Get the state.
   // 3. Listen to changes on the state.
   // 4. Update the state
 
-  let state
+  let state;
+  let listeners = []
 
-  const getState = () => state
 
-  return {
-    getState
+
+  const getState = () => state;
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+
   }
+  return {
+    getState,
+    subscribe
+  };
 }
+
+const store = createStore();
+store.subscribe(() => {
+  console.log("The new state is", store.getState());
+});
+store.subscribe(() => {
+  console.log("The store changed");
+});
