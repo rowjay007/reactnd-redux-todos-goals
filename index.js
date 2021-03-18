142e91811d4527168e7207fd12d403072eca5a3c
rowjay007 <rowlives@gmail.com>
2021-03-18 16:17:40 +0100

    create dispatch for action creators

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+
+function todos  (state = [], action ) => {
+  if(action.type ===   'ADD_TODO'){
+    return state.concat([action.todo])
+  }
+  return state
+}
+
 function createStore() {
   // The store should have four parts
   // 1. The state
@@ -6,19 +14,26 @@ function createStore() {
   // 4. Update the state
 
   let state;
-  let listeners = []
-
-
+  let listeners = [];
 
   const getState = () => state;
 
   const subscribe = (listener) => {
-    listeners.push(listener)
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
 
+
+  const dispatch = (action) =>  {
+    state = todos(state,action)
+    listeners.forEach((listener) => listener())
   }
   return {
     getState,
-    subscribe
+    subscribe,
+    dispatch
   };
 }
 
@@ -26,6 +41,6 @@ const store = createStore();
 store.subscribe(() => {
   console.log("The new state is", store.getState());
 });
-store.subscribe(() => {
+const unsubscribe = store.subscribe(() => {
   console.log("The store changed");
 });
