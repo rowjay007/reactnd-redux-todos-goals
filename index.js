db30a80e99897e17a7df706f8ebe9bd45c9516ea
rowjay007 <rowlives@gmail.com>
2021-03-19 07:58:19 +0100

    create goals action type

--- index.js
+++ index.js
@@ -33,14 +33,35 @@ function createStore(reducer) {
 }
 
 function todos  (state = [], action ) => {
-  if(action.type ===   'ADD_TODO'){
-    return state.concat([action.todo])
+  switch(action.type){
+    case 'ADD_TODO':
+      return state.concat([action.tod])
+      case 'REMOVE_TODO':
+      return state.filter((todo) => todo.id !== action.id)
+      case 'TOGGLE_TODO':
+        return state.map((todo) => todo.id !== action.id ? todo : 
+        Object.assign({}, todo, {complete: !todo.complete}))
+        default :
+        return state
   }
-  return state
-}
-const store = createStore();
-store.subscribe(() => {
-  console.log("The new state is", store.getState());
+
+
+  function goals (state = [], action) => {
+    switch(action.type){
+      case 'ADD_GOAL':
+        return state.concat([action.goal])
+        case 'REMOVE_GOAL': 
+        return state.filter((goal) => goal.id  !== action.id)
+        default :
+        return state
+    }
+    
+  }
+
+  
+  const store = createStore();
+  store.subscribe(() => {
+    console.log("The new state is", store.getState());
 });
 
 store.dispatch({
